336772eb4a8559ea293f41aebe3c30ca6d4a1482
zhu_sk <zhu_sk@sina.com>
2022-10-23 20:43:18 +0800

    更新：manwa 域名更新

--- tampermonkey/manwa.user.js
+++ tampermonkey/manwa.user.js
@@ -1,14 +1,14 @@
 // ==UserScript==
 // @name         manwa图片下载
 // @namespace    https://github.com/coofo/someScript
-// @version      0.2.5
+// @version      0.2.6
 // @license      AGPL License
 // @description  下载
 // @author       coofo
 // @updateURL    https://github.com/coofo/someScript/raw/main/tampermonkey/manwa.user.js
 // @downloadURL  https://github.com/coofo/someScript/raw/main/tampermonkey/manwa.user.js
 // @supportURL   https://github.com/coofo/someScript/issues
-// @include      /^https://manwa.(me|live|vip|fun)/book/\d+/
+// @include      /^https://manwa.(me|live|vip|fun|one|pro)/book/\d+/
 // @require      https://cdn.jsdelivr.net/npm/sweetalert2@11
 // @require      https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.5/jszip.min.js
 // @require      https://cdnjs.cloudflare.com/ajax/libs/crypto-js/4.1.1/crypto-js.min.js
@@ -19,6 +19,8 @@
 // @connect      img.manwa.live
 // @connect      img.manwa.vip
 // @connect      img.manwa.fun
+// @connect      img.manwa.one
+// @connect      img.manwa.pro
 // @grant        GM_download
 // @grant        GM_xmlhttpRequest
 // @grant        GM_setValue
